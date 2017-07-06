9809140c47ccc33d0870a5f306721e3562f2898e
Thomas Garnier <thgarnie@google.com>
2017-07-06 09:39:47 -0700

    x86/paravirt: Adapt assembly for PIE support
    if PIE is enabled, switch the paravirt assembly constraints to be
    compatible. The %c/i constrains generate smaller code so is kept by
    default.
    
    Position Independent Executable (PIE) support will allow to extended the
    KASLR randomization range below the -2G memory limit.
    
    Signed-off-by: Thomas Garnier <thgarnie@google.com>

--- arch/x86/include/asm/paravirt_types.h
+++ arch/x86/include/asm/paravirt_types.h
@@ -337,9 +337,17 @@ extern struct pv_lock_ops pv_lock_ops;
 #define PARAVIRT_PATCH(x)					\
 	(offsetof(struct paravirt_patch_template, x) / sizeof(void *))
 
+#ifdef CONFIG_X86_PIE
+#define paravirt_opptr_call "a"
+#define paravirt_opptr_type "p"
+#else
+#define paravirt_opptr_call "c"
+#define paravirt_opptr_type "i"
+#endif
+
 #define paravirt_type(op)				\
 	[paravirt_typenum] "i" (PARAVIRT_PATCH(op)),	\
-	[paravirt_opptr] "i" (&(op))
+	[paravirt_opptr] paravirt_opptr_type (&(op))
 #define paravirt_clobber(clobber)		\
 	[paravirt_clobber] "i" (clobber)
 
@@ -395,7 +403,7 @@ int paravirt_disable_iospace(void);
  */
 #define PARAVIRT_CALL					\
 	ANNOTATE_RETPOLINE_SAFE				\
-	"call *%c[paravirt_opptr];"
+	"call *%" paravirt_opptr_call "[paravirt_opptr];"
 
 /*
  * These macros are intended to wrap calls through one of the paravirt
