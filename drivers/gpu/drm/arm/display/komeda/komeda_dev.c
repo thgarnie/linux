7e8d4fcf8daf6bc40a7ee2f4755432395541e108
Liviu Dudau <Liviu.Dudau@arm.com>
2019-01-15 10:06:31 +0000

    drm: arm/komeda: Remove IRQ parsing from initial series
    The initial series is only introducing the basic components and not
    implementing IRQ handling. Remove the left over code that touches
    IRQs until the proper implementation is introduced in a later series.
    
    Reviewed-by: James Qian Wang (Arm Technology China) <james.qian.wang@arm.com>
    Signed-off-by: Liviu Dudau <liviu.dudau@arm.com>

--- drivers/gpu/drm/arm/display/komeda/komeda_dev.c
+++ drivers/gpu/drm/arm/display/komeda/komeda_dev.c
@@ -59,11 +59,6 @@ static int komeda_parse_dt(struct device *dev, struct komeda_dev *mdev)
 		return PTR_ERR(clk);
 
 	mdev->mclk = clk;
-	mdev->irq  = platform_get_irq(pdev, 0);
-	if (mdev->irq < 0) {
-		DRM_ERROR("could not get IRQ number.\n");
-		return mdev->irq;
-	}
 
 	for_each_available_child_of_node(np, child) {
 		if (of_node_cmp(child->name, "pipeline") == 0) {
